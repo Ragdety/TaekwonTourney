01177fb55913fe7db7abfb2e302fd78c1969fcd2
Edgar Terrazas <Ragdety@users.noreply.github.com>
2021-09-29 15:30:59 -0400

    Added basic setup files

---
+++ TaekwonTourney.Contracts/v1/ApiRoutes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaekwonTourney.Contracts.v1
+{
+	public static class ApiRoutes
+	{
+		public const string Root = "api";
+		public const string Version = "v1";
+		public const string Base = Root + "/" + Version;
+
+		public const string Home = Base + "/home";
+	}
+}
---
+++ src/TaekwonTourney.API/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaekwonTourney.Contracts.v1;
+
+namespace TaekwonTourney.API.Controllers
+{
+	[ApiController]
+	public class HomeController : ControllerBase
+	{
+		[HttpGet(ApiRoutes.Home)]
+		public IActionResult Home()
+		{
+			return Ok("Welcome Home");
+		}
+	}
+}
