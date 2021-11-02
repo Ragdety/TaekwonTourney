eeb94f2d18274188ee2010c1ff1f1e7452996477
Paige Craig <paigecraig01@yahoo.com>
2021-11-02 11:09:52 -0400

    implimented participant controllers

--- src/TaekwonTourney.Core/DomainObjects/DomainModels/ParticipantCreationModel.cs
+++ src/TaekwonTourney.Core/DomainObjects/DomainModels/ParticipantCreationModel.cs
@@ -1,7 +1,14 @@
-﻿namespace TaekwonTourney.Core.DomainObjects.DomainModels
+﻿using System;
+using TaekwonTourney.Core.Enums;
+
+namespace TaekwonTourney.Core.DomainObjects.DomainModels
 {
     public class ParticipantCreationModel
     {
-        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public BeltLevel BeltLevel { get; set; }
+        public BlackBeltLevel? BlackBeltLevel { get; set; }
     }
 }
