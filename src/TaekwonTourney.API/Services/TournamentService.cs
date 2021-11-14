c2ca1f80ab407b82d2bd7cb062c2e918ad2cb7f0
vschmick <vincentschmick@gmail.com>
2021-11-13 21:17:12 -0500

    Fixed problem in TournamentServices

--- src/TaekwonTourney.API/Services/TournamentService.cs
+++ src/TaekwonTourney.API/Services/TournamentService.cs
@@ -60,8 +60,8 @@ namespace TaekwonTourney.API.Services
                 return new TournamentResponse("Tournament not found.");
 
             existingTournament.TournamentName = tournament.TournamentName;
-            //existingTournament.StartDate = Tournament.StartDate;
-            //existingTournament.EndDate = Tournament.EndDate;
+            existingTournament.StartDate = tournament.StartDate;
+            existingTournament.EndDate = tournament.EndDate;
             existingTournament.TournamentType = tournament.TournamentType;
 
             try
