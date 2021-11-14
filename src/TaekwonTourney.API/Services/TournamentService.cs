22525ed1884b2821db0fe9078ba2d826e59fa9a3
vschmick <vincentschmick@gmail.com>
2021-11-13 20:55:23 -0500

    Added start date and end date for Tournament

--- src/TaekwonTourney.API/Services/TournamentService.cs
+++ src/TaekwonTourney.API/Services/TournamentService.cs
@@ -60,7 +60,8 @@ namespace TaekwonTourney.API.Services
                 return new TournamentResponse("Tournament not found.");
 
             existingTournament.TournamentName = tournament.TournamentName;
-            existingTournament.TournamentDate = tournament.TournamentDate;
+            //existingTournament.StartDate = Tournament.StartDate;
+            //existingTournament.EndDate = Tournament.EndDate;
             existingTournament.TournamentType = tournament.TournamentType;
 
             try
